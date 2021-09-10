13137778f8e031f3e07ad88ab9c380dd063bd976
Wendelin Gerein <info@webdelin.de>
2021-09-10 15:23:06 +0200

    create ts vue3 and add tailwind

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-no-wrap bg-gray-200">
+    <the-sidebar></the-sidebar>
+    <router-view />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import TheSidebar from "@/components/TheSidebar.vue";
+export default defineComponent({
+  components: { TheSidebar },
+  setup() {},
+});
+</script>
---
+++ src/hooks/fetch.ts
@@ -0,0 +1,10 @@
+import {ref} from "vue"
+export function menuFetch(url:RequestInfo, options?:RequestInit){
+  const response = ref(null)
+
+  const request = async () => {
+    const res = await fetch(url, options)
+    response.value = res.json()
+  }
+  return {response, request}
+}
